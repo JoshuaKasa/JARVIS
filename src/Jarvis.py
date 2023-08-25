b4913d21e08ff2647a85e3b583ba1e8414d59560
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2023-08-26 00:40:17 +0200

    Added exception handling and modularity

--- src/Jarvis.py
+++ src/Jarvis.py
@@ -1,136 +1,12 @@
-import json
 import openai
 import pyttsx3
-import geocoder
-import requests
-import datetime
-import keyboard
-import speedtest
-import pywhatkit
-import webbrowser
-import subprocess
 
 import speech_recognition as sr
 
 from pydub import AudioSegment
 from pydub.playback import play
 from JarvisPrompt import jarvis_prompt
-
-# Functions for the assistant to use
-def execute_speedtest():
-    speed = speedtest.Speedtest()
-
-    speedtest_info = {
-        "download": speed.download(),
-        "upload": speed.upload(),
-        "ping": speed.results.ping,
-    }
-
-    return json.dumps(speedtest_info)
-
-def execute_cmd_command(command):
-    result = subprocess.run(
-        command,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE,
-        shell = True,
-        text = True
-    )
-
-    function_info = {
-        "name": "execute_cmd_command",
-        "description": "Executes an OS command",
-        "command": command,
-        "stdout": result.stdout,
-        "stderr": result.stderr,
-        "returncode": result.returncode,
-    }
-
-    return json.dumps(function_info)
-
-def open_website(url):
-    webbrowser.open(url)
-
-    function_info = {
-        "name": "open_website",
-        "description": "Opens a website",
-        "url": url,
-    }
-
-    return json.dumps(function_info)
-
-def exit_program():
-    exit()
-
-def get_current_weather():
-    # Getting my current location
-    g = geocoder.ip("me")
-    city = g.city
-
-    # Getting the weather data
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    with open("../WEATHER.txt", "r") as f:
-        api_key = f.read()
-    url = base_url + "appid=" + api_key + "&q=" + city
-
-    response = requests.get(url)
-    weather_data = response.json()
-
-    # Modifying information for GPT to understand
-    weather_info = {
-        "city": city,
-        "temperature": weather_data["main"]["temp"] - 273.15, # in Celsius
-        "feels_like": weather_data["main"]["feels_like"],
-        "humidity": weather_data["main"]["humidity"],
-        "pressure": weather_data["main"]["pressure"],
-        "wind_speed": weather_data["wind"]["speed"],
-        "wind_direction": weather_data["wind"]["deg"],
-        "weather": weather_data["weather"][0]["main"],
-        "weather_description": weather_data["weather"][0]["description"],
-    }
-
-    return json.dumps(weather_info)
-
-def get_current_time():
-    current_time = datetime.datetime.now().strftime("%H:%M:%S")
-
-    return current_time
-
-def send_whatsapp_message(phone_number_key, message, delay_minutes=1):
-    # Define a dictionary for phone number mappings
-    phone_numbers = {
-        "girlfriend": 0,
-        "mom": 1,
-        "giovanni": 2,
-    }
-
-    # Read phone numbers from the file
-    with open("../PHONENUMBERS.txt", "r") as f:
-        numbers_list = f.read().split("\n")
-
-    # Get the corresponding phone number from the dictionary
-    if phone_number_key in phone_numbers:
-        phone_number = numbers_list[phone_numbers[phone_number_key]]
-    else:
-        raise ValueError("Invalid phone number key")
-
-    # Get current time
-    current_time = datetime.datetime.now()
-    time_hour = current_time.hour
-    time_minute = current_time.minute
-
-    # Sending the message with the specified delay
-    pywhatkit.sendwhatmsg(phone_number, message, time_hour, time_minute + delay_minutes)
-
-    # Pressing enter to send the message
-    keyboard.press_and_release("enter")
-
-    function_info = {
-        "phone_number": phone_number,
-        "message": message,
-    }
-
-    return json.dumps(function_info)
+from functions.Functions import *
 
 # Define a dictionary to map function names to their implementations
 available_functions = {
@@ -215,7 +91,7 @@ functions = [
         "parameters": {
             "type": "object",
             "properties": {
-                "phone_number": {
+                "phone_number_key": {
                     "type": "string",
                     "enum": ["girlfriend", "mom", "giovanni"]
                 },
@@ -258,7 +134,7 @@ engine.runAndWait()
 
 while True:
 
-    user_message = "" # Resetting the user message
+    user_message: str = "" # Resetting the user message
     # Getting the user speech
     try:
         with sr.Microphone() as source: # Using the microphone as the audio source
